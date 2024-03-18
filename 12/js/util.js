53bd0f7ad5975fa7d2dc9f0b1d4e7625dad68da9
Keks <keks@htmlacademy.ru>
2024-03-18 15:29:16 +0300

    :heavy_check_mark: Сборка #12

---
+++ 12/js/util.js
@@ -0,0 +1,29 @@
+
+const getRandomInteger = (min, max) => {
+  min = Math.ceil(Math.min(min, max));
+  max = Math.floor(Math.max(min, max));
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+const createObjectsArray = (addObject, arraySize) => {
+  const objectsArray = [];
+  for (let i = 0; i < arraySize; i++) {
+    objectsArray.push(addObject());
+  }
+  return objectsArray;
+};
+
+const numDecline = (num, nominative, genitiveSingular, genitivePlural) => {
+  if (num % 10 === 0 || num % 100 > 4 && num % 100 < 21) {
+    return genitivePlural;
+  }
+  return num % 10 === 1
+    ? nominative
+    : genitiveSingular;
+};
+
+const isEnterKey = (evt) => evt.key === 'Enter';
+
+const isEscapeKey = (evt) => evt.key === 'Escape';
+
+export { getRandomInteger, createObjectsArray, isEscapeKey, isEnterKey, numDecline };
